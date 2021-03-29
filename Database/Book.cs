4cfdfacc08117355275e6545606fd63cfc65ee73
Ryan <ryanwaszak@gmail.com>
2021-03-28 20:23:14 -0500

    create API models

---
+++ Database/Book.cs
@@ -0,0 +1,12 @@
+namespace Database
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool Published { get; set; }
+        public string Genre { get; set; }
+        public string AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
