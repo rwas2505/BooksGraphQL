b33ce72a00b716a57660d1711b3d8b9bca19546d
Ryan <ryanwaszak@gmail.com>
2021-03-28 20:17:28 -0500

    disable https redirection

--- Startup.cs
+++ Startup.cs
@@ -35,12 +35,7 @@ namespace BooksGraphQL
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
+            
             app.UseMvc();
         }
     }
