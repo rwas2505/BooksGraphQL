f66c2d795dcc642cd40f7e9f4c61331abbc5db8d
Ryan <ryanwaszak@gmail.com>
2021-03-28 20:38:06 -0500

    add graphQL middleware

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Database;
+using GraphiQl;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -43,6 +44,7 @@ namespace BooksGraphQL
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseGraphiQl("/graphql");
             app.UseMvc();
         }
     }
