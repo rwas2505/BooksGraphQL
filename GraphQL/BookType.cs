53f147d5e92ee76c23c98139e52d1c1483248b12
Ryan <ryanwaszak@gmail.com>
2021-03-28 20:55:02 -0500

    add graphQL queries

---
+++ GraphQL/BookType.cs
@@ -0,0 +1,18 @@
+using Database;
+using GraphQL.Types;
+
+namespace GraphQL
+{
+    public class BookType : ObjectGraphType<Book>
+    {
+        public BookType()
+        {
+        Name = "Book";
+
+        Field(x => x.Id, type: typeof(IdGraphType)).Description("The ID of the Book.");
+        Field(x => x.Name).Description("The name of the Book");
+        Field(x => x.Genre).Description("Book genre");
+        Field(x => x.Published).Description("If the book is published or not");
+        }
+    }
+}
