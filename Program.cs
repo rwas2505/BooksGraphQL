378739c5e11948ab24a6942a1907544d1bfe864c
Ryan <ryanwaszak@gmail.com>
2021-03-28 20:33:48 -0500

    seed the db

--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Database;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace BooksGraphQL
@@ -14,11 +16,42 @@ namespace BooksGraphQL
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+        IWebHost host = CreateWebHostBuilder(args).Build();
+        
+        using (IServiceScope scope = host.Services.CreateScope())
+        {
+            ApplicationDbContext context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            var authorDbEntry = context.Authors.Add(
+            new Author
+            {
+                Name = "First Author",
+            });
+
+            context.SaveChanges();
+
+            context.Books.AddRange(
+            new Book
+            {
+                Name = "First Book",
+                Published = true,
+                AuthorId = authorDbEntry.Entity.Id,
+                Genre = "Mystery"
+            },
+            new Book
+            {
+                Name = "Second Book",
+                Published = true,
+                AuthorId = authorDbEntry.Entity.Id,
+                Genre = "Crime"
+            });
+        }
+        
+        host.Run();
+    }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +27,11 @@ namespace BooksGraphQL
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(context =>
+            {
+                context.UseInMemoryDatabase("OktaGraphQL");
+            });
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -35,7 +42,7 @@ namespace BooksGraphQL
             {
                 app.UseDeveloperExceptionPage();
             }
-            
+
             app.UseMvc();
         }
     }
